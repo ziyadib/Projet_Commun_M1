d35ad0a71d6d288cf41eb15419ae4e9cdc508af5
bader <bader@MacBook-Pro-de-bader.local>
2017-06-09 17:52:41 +0200

    Amelioration Import Quiz
    V0

--- FrontEnd/VueProf/app/controllers/ImportQuizController.js
+++ FrontEnd/VueProf/app/controllers/ImportQuizController.js
@@ -1,12 +1,18 @@
-/**
- * Created by Victor on 21/02/2016.
- */
 
 angular.module('RevisatorProfApp')
     .controller('ImportQuizController', function ($scope, $http, sharedStorageService, $location, LxNotificationService) {
-        var texteintroductif = "Ce module a pour objectif de vous permettre d'importer vos propres quiz merci de respecter \n le pattern ci-dessous " +$scope.quiz;
+      //texte explicatif pour l'utilisateur
+        var texteintroductif = "Ce module a pour objectif de vous permettre d'importer vos propres quiz merci de respecter \n le pattern ci-dessous:" ;
+        var patternTitle = "{\"title\":\"le titre de ce quizz est pattern\",";
+        var patternQuestion ="\"questions\":[{\"questionTitle\":\"titre question 1\",\"questionAnswer\":{\"1\":\"reponse question 1\",\"2\":\"reponse 2 question 1\"},\"correctAnswer\":{\"1\":false,\"2\":true},\"nombreReponse\":2},{\"questionTitle\":\"titre question 2\",\"questionAnswer\":{\"1\":\"reponse 1 question 2\",\"2\":\"reponse 2 question 2\"},\"correctAnswer\":{\"2\":true},\"nombreReponse\":2}],";
+        var patternComplementaire ="\"year\":{\"idNiveau_etude\":3,\"niveau\":\"BAC +4\"},\"matiere\":{\"nom\":\"Nom matiere du quizz\"},\"nbOfQuestions\":2}";
         $scope.explicationsDetaillees =texteintroductif;
-        $scope.test ="toz";
+        $scope.patternTitle = patternTitle;
+        $scope.patternQuestion=patternQuestion;
+        $scope.patternComplementaire=patternComplementaire;
+
+
+
     $scope.add = function() {
         var f = document.getElementById('file').files[0],
             r = new FileReader();
